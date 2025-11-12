295e54b05865e098cf18c404421a7adedb88f811
MAC <mac@Victor-Joeys-MacbookPro.local>
2025-11-12 12:12:38 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../globals.css"
+
+export default function Index() {
+  return (
+    <View
+className="flex-1 items-center justify-center"
+    >
+      <Text className="text-5xl">Hi, Welcome.</Text>
+    </View>
+  );
+}
